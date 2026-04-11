8210d3d385b880ccc1433ebaf2fcdffe2f1f6eab
unknown <Administrator@CORP.PCXLAB.COM>
2026-04-12 02:06:18 +0530

    General functiosn updated

---
+++ [99] General/Create-PCXCMPckage.ps1
@@ -0,0 +1,36 @@
+﻿function Create-PCXCMPackage {
+    param (
+        [parameter(mandatory=$true, Position=0)] [string]$PackageName,
+        [parameter(Mandatory=$false, Position=1)] [string]$company,
+        [parameter(Mandatory=$false, Position=2)] [string]$version = "1.0.0",
+        [parameter(Mandatory=$false, Position=3)] [string]$language = "EN-US",
+        [parameter(mandatory=$false, Position=4)] [string]$Path = "\\192.168.25.214\Package_Source\",
+        [parameter(mandatory=$false, Position=5)] [string]$Description = "Auto-created package"
+    )
+
+    Write-PCXLog "Starting package creation: $PackageName"
+
+    try {
+        $pkg = New-CMPackage -Name $packagename -Description $Description -Manufacturer $company -Version $version -Language $language -Path $path -ErrorAction Stop
+
+        Write-PCXLog "Package created successfully: $PackageName"
+
+        return $pkg
+    }
+    catch {
+        Write-PCXLog "Failed to create package: $PackageName | Error: $_" "ERROR"
+        throw
+    }
+}
+
+<#Usage Example
+Create-PCXCMPackage -packagename "PKGS_7zip_26.00" -company "Igor_Pavlov" -version "12.1.0" -language "EN-IN" -path "\\192.168.25.214\Package_Source\Applications\Igor_Pavlov\7zip\7zip_2.0.0"
+Create-PCXCMPackage -packagename "PKGS_7zip_26.00" -company "Igor_Pavlov" -version "12.1.0" -language "EN-IN"
+Create-PCXCMPackage -packagename "PKGS_7zip_26.00" -company "Igor_Pavlov" -version "12.1.0"
+Create-PCXCMPackage -packagename "PKGS_7zip_26.00"
+
+# IMPORTENT : Deletion commad just to retest it again so that we need not to delete from consolve
+Remove-CMPackage -Name "PKGS_7zip_26.00"
+#>
+
+
---
+++ [99] General/Write-PCXLog.ps1
@@ -0,0 +1,29 @@
+﻿function Write-PCXLog {
+    param (
+        [parameter(mandatory=$false, Position=0)] [string]$Message,
+        [parameter(mandatory=$false, Position=1)] [string]$Level = "INFO",
+        [parameter(mandatory=$false, Position=2)] [string]$LogPath = "C:\Temp\PCXCM.log"
+    )
+
+    # Timestamp and log details
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "$timestamp [$Level] $Message"
+
+    # Test and create folder if dosenot exist
+    $logDir = Split-Path $LogPath
+    if (!(Test-Path $logDir)) {
+        New-Item -ItemType Directory -Path $logDir -Force | Out-Null
+    }
+    
+    # Write information to the log file
+    Write-Output $logEntry
+    Add-Content -Path $LogPath -Value $logEntry
+}
+
+<#Usage Examle 
+Write-PCXLog -Message "Hellowaaaa" -Level "Error" -LogPath "C:\Temp\PCXCM.log"
+Write-PCXLog -Message "Idlivada" -Level "Warning" -LogPath "C:\Temp\PCXCM.log"
+Write-PCXLog -Message "Mangalapuram" -Level "Warning" -LogPath "C:\Temp\PCXCM.log"
+Write-PCXLog -Message "Hi" -Level "Warning"
+Write-PCXLog -Message "Bye"
+#>
